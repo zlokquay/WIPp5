983abad4b665a902fe0d26f25f191acbf4b68ec3
Zlokquay <zataylor@smcm.edu>
2018-03-09 10:38:49 -0500

    plans to rewrite, but here ya go

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,45 @@
+let button;
+let multiplier = 1.15;
+let goldCounter = 0;
+let goldMinerCount = 0;
+let goldMinerCost = 0;
+
 function setup() {
-  // put setup code here
+  createCanvas(600, 600);
+  button = createButton('Generate Gold');
+  button.position(20, 20);
+  button.mousePressed(generateGold);
+
+  button = createButton('Gold Miner');
+  button.position(250, 20);
+  button.mousePressed(goldMiner);
 }
 
 function draw() {
-  // put drawing code here
+  background(255);
+  textSize(16);
+  text(floor(goldCounter), 150, 40);
+  text(goldMinerCount, 400, 40);
+  text(goldMinerCost, 400, 80)
+  goldMinerCost = floor(20 * (Math.pow(multiplier, goldMinerCount)));
+  if(goldMinerCount > 0)
+    autoGenerateGold();
+}
+
+function generateGold(){
+  goldCounter++;
+}
+
+function goldMiner(){
+  if(goldCounter >= goldMinerCost){
+    goldCounter = goldCounter - goldMinerCost;
+    goldMinerCount++;
+  }
+}
+
+function autoGenerateGold (){
+  let effectiveness = 0.5;
+  if(goldMinerCount >= 10)
+    effectiveness = 1;
+  goldCounter = goldCounter + (goldMinerCount * (effectiveness/frameRate()));
 }
